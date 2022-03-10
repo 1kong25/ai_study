d188d3fca82b9ea9f8bfa81d252928b516bba104
1kong25 <kjh001474@gmail.com>
2022-03-11 01:46:38 +0900

    preprocessing

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'
-config = ('-l kor+eng --oem 3 --psm 11') 
-print(pytesseract.image_to_string("/Users/kjh001/Downloads/aitest.jpeg",config=config))
-
+config = ('-l kor+eng --oem 3 --psm 4') # psm은 page segmentation modes 5x /best:4(구조 그대로 나옴), 11(인식한 거 한 줄로 나옴)
+print(pytesseract.image_to_string("/Users/kjh001/Desktop/study/ai_tes_pic/aitest.jpeg",config=config))
